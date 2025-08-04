5ca9040dc57c18cd9f7c42f8b09337aa4fdccfd1
refardian <92292301+refardian@users.noreply.github.com>
2025-08-04 18:32:51 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"68909a2600a1ed001f4e4498",
+				"68909a2600a1ed001f4e449b"
+			]
+		},
+		"68909a2600a1ed001f4e4498": {
+			"id": "68909a2600a1ed001f4e4498",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"68909a2600a1ed001f4e449b": {
+			"id": "68909a2600a1ed001f4e449b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
